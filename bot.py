219bee38aefee3b8f462083afc29f7e929a41dc1
Nimesh Akalanka <90953455+Mafia58@users.noreply.github.com>
2023-07-08 00:23:40 +0530

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,33 @@
+from pyrogram import Client, __version__
+from pyrogram.raw.all import layer
+from utils import Media
+from info import API_ID, API_HASH, BOT_TOKEN
+from pyromod import listen
+
+class Bot(Client):
+
+    def __init__(self):
+        super().__init__(
+            "ftv",
+            api_id=API_ID,
+            api_hash=API_HASH,
+            bot_token=BOT_TOKEN,
+            workers=50,
+            plugins={"root": "plugins"},
+            sleep_threshold=5,
+        )
+
+    async def start(self):
+        await super().start()
+        await Media.ensure_indexes()
+        me = await self.get_me()
+        self.username = '@' + me.username
+        print(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
+
+    async def stop(self, *args):
+        await super().stop()
+        print("Bot stopped. Bye.")
+
+
+app = Bot()
+app.run()
---
+++ info.py
@@ -0,0 +1,34 @@
+# Bot information
+API_ID = 8623612
+API_HASH = "06ea2889c5517eb64017b032d667e29f"
+BOT_TOKEN = "6186428853:AAGjhILlrYsXHBPD5EZu3OdVqRJw3KxLAMw"
+
+# Bot settings
+CACHE_TIME = 300
+USE_CAPTION_FILTER = True
+
+# Admins, Channels & Users
+ADMINS = 1853733618
+AUTH_USERS = [1853733618, 1157165730]
+AUTH_CHANNEL = -1001968698233
+AUTH_GROUPS = -1001978585409
+CHANNELS = [-1001887970234, -1001788418243, -1001915105063, -1001960744686, -1001749057548, -1001396095544]
+# MongoDB information
+DATABASE_URI = "mongodb+srv://nimesh:nimesh@cluster0.hf6yj7l.mongodb.net/?retryWrites=true&w=majority"
+DATABASE_NAME = "Cluster0"
+COLLECTION_NAME = "Telegram_files"
+
+# start Pics
+PICS = "https://telegra.ph/file/040950a00143b3a989fee.png"
+
+# Messages
+start_msg = """
+**Hi, I'm FTv Movie Filter Bot**
+
+I'm an advanced auto filter bot😏
+You can use me in your groups.(make me admin)
+"""
+START_MSG = start_msg
+
+CUSTOM_FILE_CAPTION = "@FTv4U"
+API_KEY = "d2ffa66e"
